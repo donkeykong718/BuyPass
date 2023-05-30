ba80cd4896917fa0ebe7f15a408316164ca2969e
donkeykong718 <122040169+donkeykong718@users.noreply.github.com>
2023-05-30 18:52:19 -0400

    Links to homepage automatically. Also adds sound. And it works.

---
+++ buypass/src/app/home/layout.jsx
@@ -0,0 +1,7 @@
+import "../globals.css";
+
+export default function Layout({ children }) {
+  return (
+    <main className="h-[100vh] text-center font-Bookerly">{children}</main>
+  );
+}
--- buypass/src/app/page.js
+++ buypass/src/app/page.js
@@ -4,32 +4,51 @@ import React, { useState, Suspense } from "react";
 
 import Logo from "./components/Logo";
 import Header from "./components/Header";
-import SearchCol from "./components/SearchCol";
-import AmznResCol from "./components/AmznResCol";
-import GglResModal from "./components/GglResModal";
+// import SearchCol from "./components/SearchCol";
+// import AmznResCol from "./components/AmznResCol";
+// import GglResModal from "./components/GglResModal";
 import Loader from "./components/Loader";
+import Results from "./components/Results";
 
-export const BrandContext = React.createContext();
+// export const BrandContext = React.createContext();
 export const GLoadingContext = React.createContext();
-export const ModalContext = React.createContext();
-export const SearchContext = React.createContext();
-export const SearchTermContext = React.createContext();
+// export const ModalContext = React.createContext();
+// export const SearchContext = React.createContext();
+// export const SearchTermContext = React.createContext();
 
 export default function Main() {
-  const [brand, setBrand] = useState(null);
+  // const [brand, setBrand] = useState(null);
   const [gLoading, setGLoading] = useState(false);
-  const [modal, setModal] = useState(false);
-  const [search, setSearch] = useState(false);
-  const [searchTerm, setSearchTerm] = useState(null);
+  // const [modal, setModal] = useState(false);
+  // const [search, setSearch] = useState(false);
+  // const [searchTerm, setSearchTerm] = useState(null);
 
   return (
     <main className="text-center font-Bookerly">
-      <div className=" bg-white min-h-[25%] max-h-[32%]">
+      <div className="bg-white min-h-[25%] max-h-[32%]">
         <Logo />
         <Header />
       </div>
 
-      <SearchContext.Provider value={{ search, setSearch }}>
+      <GLoadingContext.Provider value={{ gLoading, setGLoading }}>
+        {gLoading && (
+          <Loader />
+          // <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+          //   <iframe
+          //     width="560"
+          //     height="315"
+          //     src="https://www.youtube.com/embed/77cMmCVBT3g?controls=0&amp;start=72"
+          //     title="YouTube video player"
+          //     frameborder="0"
+          //     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          //     allowfullscreen
+          //   ></iframe>
+          // </div>
+        )}
+        <Results />
+      </GLoadingContext.Provider>
+
+      {/* <SearchContext.Provider value={{ search, setSearch }}>
         <SearchTermContext.Provider value={{ searchTerm, setSearchTerm }}>
           <div className=" bg-white h-[10vh] w-[100%] pt-4">
             <SearchCol />
@@ -38,24 +57,20 @@ export default function Main() {
           <GLoadingContext.Provider value={{ gLoading, setGLoading }}>
             <ModalContext.Provider value={{ modal, setModal }}>
               <BrandContext.Provider value={{ brand, setBrand }}>
-                {/* bg-red-500 sm:bg-orange-500 smd:bg-yellow-500 md:bg-green-500 mdl:bg-blue-500 lg:bg-purple-500 */}
 
                 {modal ? (
-                  <div className="fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] [z-[999999] bg-white border-2 border-blue-500 m-auto h-1/2 w-1/3">
-                    Why isn't this showing up?
-                    <GglResModal />
+                  <div className="fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] [z-[999999] bg-white border-2 border-blue-500 p-3 m-auto h-1/2 w-1/3 min-w-fit z-50">
+                    <Loader />
                   </div>
                 ) : (
                   <></>
                 )}
-                <Suspense fallback={<Loader />}>
-                  {!(searchTerm == null) && <AmznResCol />}
-                </Suspense>
+                <AmznResCol />
               </BrandContext.Provider>
             </ModalContext.Provider>
           </GLoadingContext.Provider>
         </SearchTermContext.Provider>
-      </SearchContext.Provider>
+      </SearchContext.Provider> */}
     </main>
   );
 }
