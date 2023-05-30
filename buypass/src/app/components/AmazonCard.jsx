a89b20cfb1926fdd2108ef642558c66d2e6597fe
donkeykong718 <122040169+donkeykong718@users.noreply.github.com>
2023-05-30 18:59:21 -0400

    Fixes small import error

--- buypass/src/app/components/AmazonCard.jsx
+++ buypass/src/app/components/AmazonCard.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import Image from "next/image";
 import Link from "next/link";
